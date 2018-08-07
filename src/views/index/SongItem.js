9d0aa6de0860d256f334a3fc2ecc533025683c39
daniel alejandro eslava herrera <eslavadev@iMac-de-daniel.local>
2018-08-07 18:14:39 -0500

    first commit

---
+++ src/views/index/SongItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+export default class SongItem extends Component{
+    render(){
+        return(
+            <div>
+                SongItem
+            </div>
+        );
+    }
+
+
+}
+SongItem.propTypes ={
+    songId: PropTypes.string,
+    tokenPath:PropTypes.string,
+    albumPhoto:PropTypes.string,
+    songName: PropTypes.string,
+    artistName:PropTypes.string
+}
---
+++ src/views/index/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import SongItem from './SongItem';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import './index.css';
+
+class Index extends Component {
+    constructor() {
+        super();
+        this.state = {
+            song: ''
+        }
+
+    }
+    render() {
+        console.log(this.props);
+        return (
+            <div className="index">
+
+                <div className="card">
+                    <div className="card-content">
+                        <div className="index-searchBox">
+                            <input
+                                type="text"
+                                class="Index-searchBox-input"
+                                placeholder="Cancion"
+                                onChange={(e) => { this.setState({ song: e.target.value }) }}
+                                value={this.state.song}
+                            />
+                            <a
+                                className="waves-effect waves-light btn green"
+                            >
+                                <i className="fa fa-search"></i>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="card Index-results-card">
+                        <div className="card-content">
+
+                        </div>
+                </div>
+            </div>
+        )
+    }
+}
+function mapStateToProps(state){
+    return {
+        routes:state.routes
+    };
+}
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({
+
+    }, dispatch)
+}
+export default connect(mapStateToProps,mapDispatchToProps) (Index);
+
